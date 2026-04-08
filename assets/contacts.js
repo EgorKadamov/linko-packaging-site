d3c33cb0acb1a4f6b348f152628e8ba7ed143493
EgorKadamov <linkko374@gmail.com>
2026-04-09 00:40:15 +0500

    contacts.js

--- assets/contacts.js
+++ assets/contacts.js
@@ -1,34 +1,24 @@
-alert("JS работает");
+document.getElementById("contactForm").addEventListener("submit", async function (e) {
+  e.preventDefault();
 
-const form = document.getElementById("contactForm");
-alert(form ? "Форма найдена" : "Форма НЕ найдена");
+  const form = e.target;
+  const data = new FormData(form);
 
-if (form) {
-  form.addEventListener("submit", async function (e) {
-    alert("Нажат submit");
-    e.preventDefault();
-    alert("submit остановлен");
-
-    const data = new FormData(form);
-
-    try {
-      const response = await fetch(form.action, {
-        method: "POST",
-        body: data,
-        headers: {
-          "Accept": "application/json"
-        }
-      });
-
-      alert(response.ok ? "Отправка успешна" : "Ошибка ответа");
-
-      if (response.ok) {
-        window.location.href = "/thanks.html";
-      } else {
-        alert("Ошибка отправки. Попробуйте еще раз.");
+  try {
+    const response = await fetch(form.action, {
+      method: "POST",
+      body: data,
+      headers: {
+        "Accept": "application/json"
       }
-    } catch (error) {
-      alert("Ошибка отправки. Проверьте интернет.");
+    });
+
+    if (response.ok) {
+      window.location.href = "/thanks.html";
+    } else {
+      alert("Ошибка отправки. Попробуйте еще раз.");
     }
-  });
-}
+  } catch (error) {
+    alert("Ошибка отправки. Проверьте интернет.");
+  }
+});
